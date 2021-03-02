b11bbc818ea3c44ba7860be2cc5335c2a400e2ed
harish <harishmuppalla33@gmail.com>
2021-03-02 13:40:44 -0500

    Galleries

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ gulp.task('copyfonts', function() {
 });
 
 gulp.task('imagemin', function() {
-  return gulp.src('img/*.{png,jpg,gif}')
+  return gulp.src('imges/*.{png,jpg,gif}')
     .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
     .pipe(gulp.dest('dist/img'));
 });
